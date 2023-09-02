cd481c770179e3116bbb7bed35b8161ddd776641
afifi brahim <afifi71brahim@gmail.com>
2023-09-02 07:21:08 +0100

    add localstorage

--- src/contextAPI/ContextNote.jsx
+++ src/contextAPI/ContextNote.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState,useEffect } from "react";
 export const Context = createContext()
 import { useNavigate } from "react-router-dom";
 
@@ -7,14 +7,21 @@ const ContextNoteAPI = ({children})=>{
     const [state,setState]= useState({title:'',description:''})
     const [updatenode,setUpdatenode]= useState()
     const [search,setSearch]= useState('')
-    console.log(updatenode)
     const navigate = useNavigate()
+    useEffect(()=>{
+        const check = localStorage.getItem('Note')
+        if(check){
+            setNotes(JSON.parse(check))
+        }
+    },[])
     const Handlechange = (event)=>{
         setState({...state,[event.target.name]:event.target.value})
     }
     const Handleclick = ()=>{
+       
         if(state.title !=='' && state.description!==''){
             setNotes([...notes,state])
+            localStorage.setItem('Note',JSON.stringify([...notes,state]))
             navigate('/')
         }
         setState({title:'',description:''})
@@ -26,6 +33,8 @@ const ContextNoteAPI = ({children})=>{
     const Delete = (item)=>{
         const filtered = notes.filter(filte=>filte.title!==item.title)
         setNotes(filtered)
+        localStorage.setItem('Note',JSON.stringify(filtered))
+
     }
     return(
         <Context.Provider value={{ notes,setNotes,Handlechange,state,Handleclick,Update,updatenode,Delete,search,setSearch}}>
